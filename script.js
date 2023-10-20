6ba522d5dc4fc393725afe51db653d4ed6309646
Anna <Annkvelashvili@gmail.com>
2023-10-20 15:56:46 +0400

    1th_Js

---
+++ script.js
@@ -0,0 +1,24 @@
+let number = 100;
+if (number < 50) {
+  console.log("<50");
+} else if (number > 20) {
+  console.log(">20");
+} else {
+  console.log("error");
+}
+
+//Task 2
+
+let result = "მარიამ" ? "true" : "error";
+console.log(result);
+
+//Task 3
+
+let firstName = "მარიამ";
+switch (firstName) {
+  case "მარიამ":
+    console.log("true");
+    break;
+  default:
+    console.log("error");
+}
